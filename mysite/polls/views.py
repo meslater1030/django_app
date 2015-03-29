c244e3db3364b383391a4886204a144fd86132dc
Megan Slater <megan.e.slater@gmail.com>
2015-03-29 14:13:54 -0700

    finished part 2

---
+++ mysite/polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hellow, world. You're at the polls index.")
